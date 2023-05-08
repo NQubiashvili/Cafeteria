da71a9bcedc98af66511ef28dc1ee851b365537d
nika qubiashvili <nikushaqubiashvili@gmail.com>
2023-05-08 22:33:08 +0400

    Add project files.

---
+++ Cafeteria/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cafeteria.Global" Language="C#" %>
---
+++ Cafeteria/User/Profile.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace Cafeteria.User
+{
+    public partial class Profile : System.Web.UI.Page
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["userId"] == null)
+                {
+                    Response.Redirect("Login.aspx");
+                }
+                else
+                {
+                    getUserDetils();
+                }
+            }
+        }
+
+        void getUserDetils()
+        {
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("User_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", "SELECT4PROFILE");
+            cmd.Parameters.AddWithValue("@UserId", Session["userId"]);
+            cmd.CommandType = CommandType.StoredProcedure;
+            sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            rUserProfile.DataSource = dt;
+            rUserProfile.DataBind();
+            if (dt.Rows.Count == 1)
+            {
+                Session["name"] = dt.Rows[0]["Name"].ToString();
+                Session["email"] = dt.Rows[0]["Email"].ToString();
+                Session["imageUrl"] = dt.Rows[0]["ImageUrl"].ToString();
+                Session["createdDate"] = dt.Rows[0]["CreatedDate"].ToString();
+            }
+            
+        }
+
+        void getPurchaseHistory()
+        {
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Invoice", con);
+            cmd.Parameters.AddWithValue("@Action", "ORDERHISTORY");
+            cmd.Parameters.AddWithValue("@UserId", Session["userId"]);
+            cmd.CommandType = CommandType.StoredProcedure;
+            sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 0)
+            {
+                rPurchaseHistory.FooterTemplate = null;
+                rPurchaseHistory.FooterTemplate = new CostumeTemplate(ListItemType.Footer);
+            }
+            rPurchaseHistory.DataSource = dt;
+            rPurchaseHistory.DataBind();
+        }
+
+        // Costume template class to add controls to the repeater's header, item and footer section.
+        private sealed class CostumeTemplate : ITemplate
+        {
+            private ListItemType ListItemType { get; set; }
+
+            public CostumeTemplate(ListItemType type)
+            {
+                ListItemType = type;
+            }
+
+            public void InstantiateIn(Control container)
+            {
+                if (ListItemType == ListItemType.Footer)
+                {
+                    var footer = new LiteralControl("<tr><td><b>Hungry! Why not order food for you.</b><a href='Menu.aspx' class='badge badge-info ml-2'>Click to Order</a></td><tr></tbody></table>");
+                    container.Controls.Add(footer);
+                }
+            }
+        }
+
+    }
+}
