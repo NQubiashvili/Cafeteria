baf65bade584cf8ad42f512261fb1a85a11c7214
nika <43072296+NQubiashvili@users.noreply.github.com>
2023-12-15 19:35:34 +0400

    payment fixed
    fix invoice

---
+++ Cafeteria/Models/Category/CategoryModel.cs
@@ -0,0 +1,39 @@
+﻿using Cafeteria.Admin;
+using iTextSharp.text;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace Cafeteria.Models.Category
+{
+    public class CategoryModel : GetConnection 
+    {
+         protected static string Model ="Cart_Crud";
+         protected static string Actio = "SELECT";
+  /*      protected int CategoryId;
+        protected string Name;
+        protected string ImageUrl;
+        protected bool IsActive;
+        protected DateTime CreatedDate;*/
+ 
+
+
+
+
+
+        public static void GetConnectionDB()
+        {
+            GetConnectionDB(Model,Actio);
+      
+
+
+
+        }
+
+    }
+}
---
+++ Cafeteria/Models/GetConnection.cs
@@ -0,0 +1,44 @@
+﻿using Cafeteria.Admin;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.UI.WebControls;
+
+
+namespace Cafeteria.Models
+{
+    public class GetConnection
+    {
+        protected static SqlConnection con;
+        protected static SqlCommand cmd;
+        protected static SqlDataAdapter sda;
+        protected static DataTable dt;
+        protected static Repeater Data;
+
+        public static void GetConnectionDB(string List, string Action)
+        {
+          
+            
+                con = new SqlConnection(Connection.GetConnectionString());
+                cmd = new SqlCommand(List, con);
+                cmd.Parameters.AddWithValue("@Action", Action);
+                cmd.CommandType = CommandType.StoredProcedure;
+                sda = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                sda.Fill(dt);
+
+            Data.DataSource = dt;
+            Data.DataBind();
+            
+               
+                
+           
+            
+        }
+    }
+}
